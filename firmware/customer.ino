0731b5464652f7914688dca27b85ad6a34ad44fc
MikeJoshuaLopena4 <157334345+MikeJoshuaLopena4@users.noreply.github.com>
2025-09-20 22:55:15 +0800

    Update customer.ino

--- firmware/customer.ino
+++ firmware/customer.ino
@@ -1,50 +1,58 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-// WiFi credentials
 const char* ssid     = "YOUR_SSID";
 const char* password = "YOUR_PASSWORD";
 
 WiFiUDP udp;
 // Debian server IP
-//IPAddress remoteIP(192, 168, 0, 1);
-// queue server ip
-IPAddress remoteIP(xxx, xxx, xxx, xxx);
-const unsigned int remotePort = 2600;  
+IPAddress remoteIP(192, 168, 0, 105);
+const unsigned int remotePort = 2600;
 
 // Button pins
-const int buttonPins[2] = {D2, D5};
+const int buttonPins[2] = {D2, D5};  // D2 = Senior, D5 = Regular
 
 void setup() {
   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
 
-  // Wait for WiFi connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
 
-  // Set up button pins as input with internal pull-up
   for (int i = 0; i < 2; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP); // Active LOW buttons
+    pinMode(buttonPins[i], INPUT_PULLUP);
   }
-
   Serial.println("Ready to send UDP packets to Debian server...");
 }
 
 void loop() {
-  for (int i = 0; i < 2; i++) {
-    if (digitalRead(buttonPins[i]) == LOW) {
-      sendUDP(i + 1);   // Send button ID (1, 2)
-      delay(300);       // Debounce delay
-    }
+  // Auto reconnect check
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi lost... reconnecting");
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    delay(1000);
+    return;
+  }
+
+  if (digitalRead(buttonPins[0]) == LOW) {   // Senior
+    sendUDP(1);
+    delay(300);
+  }
+  if (digitalRead(buttonPins[1]) == LOW) {   // Regular
+    sendUDP(2);
+    delay(300);
   }
 }
 
-void sendUDP(int buttonID) {
-  String message = String(buttonID);
+void sendUDP(int value) {
+  String message = String(value) + "\n";  
   udp.beginPacket(remoteIP, remotePort);
   udp.write(message.c_str());
   udp.endPacket();
